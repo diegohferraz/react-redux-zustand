152c11e37a7c99d5072ad5554785b9e4c6b6a53e
Diego Ferraz <diego.ferraz@classpass.com>
2025-10-01 10:55:31 -0300

    feat: adds tailwind css config

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -4,7 +4,7 @@ export function TodoList() {
   const todos = useAppSelector(state => state.todo)
 
   return (
-    <ul>
+    <ul className="bg-amber-700 p-4 rounded shadow-md mb-4">
       {todos.map((todo, index) => (
         <li key={index}>{todo}</li>
       ))}
